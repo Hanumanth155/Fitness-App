541d7f2fdb9841977e33370517f05b0ea26bfebb
Hanumanth155 <hanumanthgouda93@gmail.com>
2025-08-18 13:05:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const RAPID_KEY = "adc5f59548msh38753e79c9506bcp1ab59ejsn2a0c10f1f974";
+const RAPID_KEY = "";
 
 const EX_BASE = "https://exercisedb.p.rapidapi.com";
 const EX_HEADERS = {
@@ -12,7 +12,7 @@ const YT_HEADERS = {
   "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com"
 };
 
-const YT_API_KEY = "AIzaSyC45iF0uz9K1CCUL3bst_-3ztcumW4x-5o";
+const YT_API_KEY = "";
 const PLACEHOLDER_IMG = "./images/fallback.gif";
 const DEBUG_CHECK = false;
 
